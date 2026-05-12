9c1d99072498c51414d874b61b8d863f084c33d6
Matthias Scheidegger <mscheidegger@gmail.com>
2026-05-12 18:57:16 +0200

    Bump version to v0.8.2-rc1

--- wrappers/kotlin/build.gradle.kts
+++ wrappers/kotlin/build.gradle.kts
@@ -102,7 +102,7 @@ mavenPublishing {
     )
     signAllPublications()
 
-    coordinates("dev.wispers", "connect", findProperty("VERSION_NAME") as String? ?: "0.8.1")
+    coordinates("dev.wispers", "connect", findProperty("VERSION_NAME") as String? ?: "0.8.2-rc1")
 
     pom {
         name.set("Wispers Connect")
