6237e61f58cf5c1f3fc7b6721dfddc6e81e85945
Matthias Scheidegger <mscheidegger@gmail.com>
2026-09-07 14:59:18 +0200

    Bump to v0.16.0

--- wrappers/kotlin/build.gradle.kts
+++ wrappers/kotlin/build.gradle.kts
@@ -106,7 +106,7 @@ mavenPublishing {
     )
     signAllPublications()
 
-    coordinates("dev.wispers", "connect", findProperty("VERSION_NAME") as String? ?: "0.16.0-rc1")
+    coordinates("dev.wispers", "connect", findProperty("VERSION_NAME") as String? ?: "0.16.0")
 
     pom {
         name.set("Wispers Connect")
